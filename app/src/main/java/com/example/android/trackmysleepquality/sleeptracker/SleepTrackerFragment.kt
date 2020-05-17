83ae1a2b453a329e455f0dfa05fe9513369c6c18
Elizabeth Adegbaju <adeotunadegbaju@gmail.com>
2020-05-17 11:30:39 +0100

    Added data binding to the recyclerview and DiffUtil to manage changes to the data instead of notifyDataSetChanged()
    Signed-off-by: Elizabeth Adegbaju <adeotunadegbaju@gmail.com>

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -59,7 +59,7 @@ class SleepTrackerFragment : Fragment() {
 
         sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
             it?.let {
-                sleepNightAdapter.data = it
+                sleepNightAdapter.submitList(it)
             }
         })
         binding.sleepList.adapter = sleepNightAdapter
